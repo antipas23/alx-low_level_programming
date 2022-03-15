5e73e4c4b4b657dca7e1c141d204190d64e79fef
antipas23 <antipasgetu123@gmail.com>
2022-03-15 17:16:25 +0300

    000

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - printing putchar 
+ * Return: 0
+**/
+int main(void)
+{
+	char *c = "_putchar ";
+
+	while (*c != ' ')
+	{
+		putchar(*c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a code to write function and nested loops
