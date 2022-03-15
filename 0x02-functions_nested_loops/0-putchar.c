e97a238a74955f120eebe6e377c8575967f4eccf
antipas23 <antipasgetu123@gmail.com>
2022-03-15 17:33:22 +0300

    00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,17 @@
  * main - printing putchar 
  * Return: 0
 **/
-
 int main(void)
 {
-	char *c = "_putchar ";
-
-	while (*c != ' ')
-	{
-		_putchar(*c);
-		c++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	
 	_putchar('\n');
 	return (0);
 }
-
