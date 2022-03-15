ccc7685e4070049d1cb82631406d04f7a015a3d3
antipas23 <antipasgetu123@gmail.com>
2022-03-16 01:31:21 +0300

    33

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,14 @@
 /**
  * _islower - checke lower
  *
+ * @c: is given
+ *
  * Return: 0 else 0
 **/
 
 int _islower(int c)
 {
-	if (c <= 'a' && c >= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
