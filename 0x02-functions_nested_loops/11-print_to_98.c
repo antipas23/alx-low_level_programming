bea8a93cae28b09919e3ddae0d9c2e5094b8f680
antipas23 <antipasgetu123@gmail.com>
2022-03-16 23:30:00 +0300

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%i,", n);
+			printf("%i", n);
 			if (n != 98)
 				printf(", ");
 		}
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%i, ", n);
+			printf("%i", n);
 			if (n != 98)
 				printf(", ");
 		}
