ae6a8874f2e7b243b037c45f5d44915d08723c2b
antipas23 <antipasgetu123@gmail.com>
2022-03-16 22:17:26 +0300

    10

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * **/
+
+void jack_bauer(void)
+{
+	char a = '0';
+	char b = '0';
+	char c = '0';
+		while(l <= 9)
+		{
+			r = "i,j,k,l";
+			_putchar(r);
+			r++;
+		}
+	_putchar(a);
+}
