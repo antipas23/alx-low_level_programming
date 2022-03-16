80fb7e4417c7bb83c5ff20b8397ca4298fef8ade
antipas23 <antipasgetu123@gmail.com>
2022-03-16 23:12:19 +0300

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - until 98
+ * @n: input number
+ * description - fun adds to integers
+**/
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n <= 98; n++)
+			printf("%i, ", n);
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+			printf("%i, ", n);
+		printf("\n");
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * times_table- 
+ * Return: 0
+ * **/
+
+
