fcac9594cffeac5709aafb34d18108b683dfa6fa
antipas23 <antipasgetu123@gmail.com>
2022-03-18 02:05:09 +0300

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * _isdigit- this is a number checker
+ *
+ * @c: this is the value of the question
+ *
+ * Return: 1 or 0
  * **/
 
 int _isdigit(int c)
+{
+	for (; c <= '9'; c++)
+	{
+		if (c <= 'z')
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
