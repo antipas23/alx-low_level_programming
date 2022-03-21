abcdfb6d86957625851e15805c254f050c384121
antipas23 <antipasgetu123@gmail.com>
2022-03-21 23:11:56 +0300

    333

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,14 +4,15 @@
  * Description - prints a tring, followed by a new line
  * *@str: function that accepts an input saved into str
  * Return: Always 0 (Success)
- *
  */
+
 void _puts(char *str)
 {
-int i = 0;
-for (i = 0; str[i] != '\0'; i++)
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
 {
-_putchar(str[i]);
+	_putchar(str[i]);
 }
 _putchar('\n');
 }
