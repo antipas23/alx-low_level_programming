4648a0e8ae1cdd5dd500d36edcb88c39d3c26e1c
antipas23 <antipasgetu123@gmail.com>
2022-03-21 22:58:05 +0300

    2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/*
- * swap_int -this is a main
+/**
+ * swap_int - this is a main
  * *@a: the first value to swap
  * *@b: the second value to swap
  * Return: 0
- */
+**/
 
 void swap_int(int *a, int *b)
 {
