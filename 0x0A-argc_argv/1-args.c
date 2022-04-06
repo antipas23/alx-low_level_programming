7cdd3b4586c4f81be458af7a864fad2f4c8e2147
antipas23 <antipasgetu123@gmail.com>
2022-04-07 00:43:25 +0300

    11

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,23 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+#include<stdio.h>
+#define UNUSED(x) (void)(x)
+
 /**
- * main - prints number of arguments
- * @argc: first parameter
- * @argv: second parameter
- *
- * Description: prints number of arguments
- * Return: 0 for code success
- */
+* main - function
+*
+* @argv: the chaine of chaine
+* @argc: the int
+* Return: Always 0.
+*/
 
 int main(int argc, char **argv)
 {
-	int sum;
+	UNUSED(argv);
+	printf("%d\n", argc - 1);
 
-	for (sum = 0; sum < argc; sum++)
-	{
-		*argv[sum] = *argv[sum];
-	}
-	printf("%d\n", (sum - 1));
 	return (0);
 }
