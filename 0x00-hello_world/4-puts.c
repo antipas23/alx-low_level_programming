ce4e07d66d31f7af70dcd34c06271f056bfc4c4b
AntipasG <antipasbelihu@gmail.com>
2023-12-16 00:36:25 +0300

    c file that prints by puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - check the code
+ *
+ *
+ *
+ *Return: Always 0
+ **/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
