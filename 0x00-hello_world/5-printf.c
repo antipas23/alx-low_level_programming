430b2b17a5d05757135e11dd27796d351bd8fbcc
AntipasG <antipasbelihu@gmail.com>
2023-12-16 00:46:19 +0300

    c file that use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - check the code
+ *
+ *
+ *Return: Always o
+ **/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
