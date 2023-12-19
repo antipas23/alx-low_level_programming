5ee0862542b8dda3e0fbbdadc59ca3f32bbdf445
antipas23 <antipasgetu123@gmail.com>
2023-12-20 02:47:10 +0300

    print alphabet Upper and Lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  *main - check the code
+  *
+  *
+  *
+  *Return: Always 0
+  **/
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
