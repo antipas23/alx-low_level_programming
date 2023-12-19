dcc369cff86d2b8360d9fe130f99b9016bdbddc8
antipas23 <antipasgetu123@gmail.com>
2023-12-20 02:41:55 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - check the code
+  *
+  *
+  *
+  *Return: Always 0
+  **/
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
