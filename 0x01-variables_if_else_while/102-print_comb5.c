0c5637b7d56a7872bb04b788a147306eb7ab0bda
antipas23 <antipasgetu123@gmail.com>
2023-12-25 17:50:06 +0300

    prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 			putchar(' ');
 			putchar(b / 10 + '0');
 			putchar(b % 10 + '0');
-			if(a < 99 || b < 99)
+			if (a < 98 || b < 99)
 			{
 				putchar(',');
 				putchar(' ');
